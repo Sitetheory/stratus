c07cc5eaea2bc8e6abfc4a520b0e340b2807d4d9
Matthew Davis <apocist@yahoo.com>
2016-07-14 16:06:51 -0700

    fixed comment issue
    Line comments must be preceded with a blank line

--- views/widgets/editor.js
+++ views/widgets/editor.js
@@ -83,8 +83,8 @@
                 unrenderEventDefault: 'blur',
 
                 // TODO: Point to CDN appropriately (PATH!)
+	            // Redactor using LESS appears to break at this time
                 requiredCssFile: [
-                    // Redactor using LESS appears to break at this time
                     Stratus.BaseUrl + 'sitetheorycore/dist/redactor/redactor.css',
                     Stratus.BaseUrl + 'sitetheorycore/dist/redactor/redactor-clips.less',
                     Stratus.BaseUrl + 'sitetheorycore/dist/codemirror/lib/codemirror.less'
