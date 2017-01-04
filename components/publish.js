5c13b998975a9835f64671d4b22623188acfeff5
chadwickmeyer <chadwick@gutensite.com>
2017-01-03 17:16:24 -0800

    adding publish component

--- components/publish.js
+++ components/publish.js
@@ -30,50 +30,75 @@
     Stratus.Components.Publish = {
         bindings: {
             ngModel: '=',
+            elementId: '@',
             versionEntity: '@',
-            showDateTimePicker: '=',
-            showUnpublish: '=',
-            showVersionHistory: '=',
+            showDateTimePicker: '@',
+            showUnpublish: '@',
+            showVersionHistory: '@',
             redirect: '@'
         },
         controller: function ($scope, $element, $parse, $attrs) {
             Stratus.Instances[_.uniqueId('publish_')] = $scope;
-            $scope.model = $parse($attrs.ngModel);
-            $scope.elementId = ($scope.elementId) ? $scope.elementId : 'publish';
-            $scope.showDataTimePicker = $scope.showDataTimePicker || false;
-            $scope.showUnpublish = $scope.showUnpublish || false;
-            $scope.showVersionHistory = $scope.showVersionHistory || false;
+            $scope.model = $scope.$parent.model;
+            $scope.versionEntity = $attrs.versionEntity || NULL;
+            //console.log('setup publish controller', $scope.model, $scope.versionEntity);
+            $scope.version = null;
+            if ($scope.versionEntity && !$scope.model.has(versionEntity)) {
+                $scope.version = $scope.model.get(versionEntity);
+                console.log('version', $scope.version);
+            }
+
+            // Options
+            $scope.elementId = $attrs.elementId || 'publish';
+            $scope.showDataTimePicker = $attrs.versionEntity || null;
+            $scope.showUnpublish = $attrs.showUnpublish || false;
+            $scope.showVersionHistory = $attrs.showVersionHistory || false;
             $scope.showMore = ($scope.showDateTimePicker || $scope.showUnpublish || $scope.showVersionHistory) ? true : false;
             // TODO: add a redirect if requested
-            $scope.redirect = $scope.redirect || false;
-            // TODO: disable/hide if this model doesn't have a versionEntity and/or timePublish
+            $scope.redirect = $attrs.redirect || false;
+
+            // Methods
+            $scope.setTimePublish = function(time) {
+                console.log('set time publish', time, $scope.version);
+                if(!$scope.version) return false;
+                time = time || 'API::NOW';
+                $scope.version.timePublish = time;
+                $scope.model.save()
+            };
+            $scope.unpublish = function() {
+                // TODO: pass in an action='unpublish' to the API
+            };
+
+            // Watchers
+            $scope.timePublish = '';
+            $scope.$watch('timePublish', $scope.setTimePublish(value));
         },
-        template: '<div class="customFontPrimary btn-group">\
+        template: '<div ng-if="model.has(versionEntity)" class="customFontPrimary btn-group">\
     <md-button aria-label="Publish" id="{{ elementId }}" class="btn btnPublish btnPublishNow"\
-        ng-click="$ctrl.model.data[versionEntity].timePublish = \'API::NOW\'; $ctrl.model.save()"\
-        ng-class="{published: $ctrl.model.data[versionEntity].isPublished == 1, unpublished: $ctrl.model.data[versionEntity].isPublished != 1}">\
+        ng-click="setTimePublish()"\
+        ng-class="{published: version.isPublished == 1, unpublished: version.isPublished != 1}">\
         <div class="btnGradientLight"></div>\
         <md-icon class="publishIcon" md-svg-src="/Api/Resource?path=@SitetheoryCoreBundle:images/icons/actionButtons/publish.svg"></md-icon>\
         <span class="publishText">Publish</span>\
     </md-button>\
-    <md-menu ng-if="$ctrl.showMore">\
+    <md-menu ng-if="showMore">\
         <div id="dropdownBtn-{{ elementId }}" class="btn btnPublish btnPublishMore">\
             <div class="btnGradientLight"></div>\
             <span class="caret"></span>\
         </div>\
         <md-menu-content class="btnPublishDropdown" aria-labelledby="dropdownBtn-{{ elementId }}">\
             <!-- TODO: after converting to dateTime component make sure these options are not needed anymore-->\
-            <stratus-date-time ng-if="$ctrl.showDateTimePicker" \
-            ng-model="$ctrl.model.data[entityVersion].timePublish"\
+            <stratus-date-time ng-if="showDateTimePicker" \
+            ng-model="timePublish"\
             data-inline="true" \
             data-collapse="true" \
             data-usecurrent="false" \
-            data-customvalue="{{ $ctrl.model.data[versionEntity] ? $ctrl.model.data[versionEntity].timePublish : 0 }}" \
+            data-customvalue="{{ timePublish || 0 }}" \
             data-style="widget"></stratus-date-time>\
-            <div ng-if="$ctrl.showUnpublish">\
-                <md-button class="action btnUnpublish" data-action="unpublish">Unpublish this Version</md-button>\
+            <div ng-if="showUnpublish">\
+                <md-button class="action btnUnpublish" ng-click="unpublish()">Unpublish this Version</md-button>\
             </div>\
-            <a ng-if="$ctrl.showRevisionHistory && $ctrl.model.data[versionEntity]" class="btnVersionHistory" href="/Content/Versions/Edit?id={{ $ctrl.model.data.id }}&versionId={{ $ctrl.model.data[versionEntity].id }}">Revision History</a>\
+            <a ng-if="showRevisionHistory && version" class="btnVersionHistory" href="/Content/Versions/Edit?id={{ model.data.id }}&versionId={{ version.id }}">Revision History</a>\
         </md-menu-content>\
     </md-menu>\
 </div>'
