0c475fe08ae92a8429b099da24c7b49a410132e3
Alex Gurrola <alexgurrola@gutensite.com>
2017-01-16 10:57:14 -0800

    Minor Repairs

--- stratus.js
+++ stratus.js
@@ -465,7 +465,7 @@
         strcmp: function (a, b) {
             a = a.toString();
             b = b.toString();
-            for (var i=0, n=Math.max(a.length, b.length); i<n && a.charAt(i) === b.charAt(i); ++i);
+            for (var i = 0, n = Math.max(a.length, b.length); i < n && a.charAt(i) === b.charAt(i); ++i);
             if (i === n) return 0;
             return a.charAt(i) > b.charAt(i) ? -1 : 1;
         },
