42529b2c3d8c0fac7364078c1afe30b7b57df15f
Gurpreet Singh <gsinghphp@seasiainfotech.com>
2017-04-07 18:32:00 +0530

    theme sel initialisation in boot

--- boot/config.js
+++ boot/config.js
@@ -145,6 +145,7 @@ boot.config({
         'stratus.components.tweet': boot.bundle + 'stratus/components/tweet' + boot.suffix,
         'stratus.components.upload': boot.bundle + 'stratus/components/upload' + boot.suffix,
         'stratus.components.visualSelector': boot.bundle + 'stratus/components/visualSelector' + boot.suffix,
+	'stratus.components.themeSelector': boot.bundle + 'stratus/components/themeSelector' + boot.suffix,
 
         // TODO: Move these to Sitetheory since they are specific to Sitetheory
         'stratus.components.permission': boot.bundle + 'stratus/components/permission' + boot.suffix,
