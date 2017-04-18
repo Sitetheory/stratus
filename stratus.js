3f41d8dd585b40233054cbc3df1fcef484c7128c
Alex Gurrola <alexgurrola@gutensite.com>
2017-04-18 16:53:41 -0700

    Minor Repairs

--- stratus.js
+++ stratus.js
@@ -3449,6 +3449,6 @@
     // ------------
 
     // Return the Stratus Object so it can be attached in the correct context as either a Global Variable or Object Reference
-    return Stratus;s
+    return Stratus;
 
 }));
