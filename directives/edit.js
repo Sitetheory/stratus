bcfe0d550b4b809f9dbf39c1abf38a30eddd3b7c
Matthew Davis <apocist@yahoo.com>
2017-06-09 12:20:10 -0700

    Edit Directive
    Initialize options for Live mode

--- directives/edit.js
+++ directives/edit.js
@@ -47,12 +47,16 @@
 
                 // Data Connectivity
                 $scope.model = null;
-                $scope.value = null;
+                $scope.value = $element.html();
 
                 if (!ngModel || !$scope.property) {
                     console.warn($scope.uid + ' has no model or property!');
                     return;
                 }
+
+                var ctrl = {
+                    initialized: false
+                };
 
                 // METHODS
 
@@ -61,24 +65,29 @@
                 };
 
                 $scope.liveEditStatus = function () {
-                    if ($scope.stratusEdit !== undefined) {
-                        return $scope.stratusEdit;
-                    } else if (Stratus.Environment.data.liveEdit !== undefined) {
-                        return Stratus.Environment.data.liveEdit;
+                    if (ctrl.initialized) {
+                        if ($scope.stratusEdit !== undefined) {
+                            return $scope.stratusEdit;
+                        } else if (Stratus.Environment.data.liveEdit !== undefined) {
+                            return Stratus.Environment.data.liveEdit;
+                        }
+                        console.warn($scope.uid + ' has no variable to track edit toggle! ($scope.stratusEdit)');
                     }
-                    console.warn($scope.uid + ' has no variable to track edit toggle! ($scope.stratusEdit)');
                     return false;
                 };
 
                 $scope.read = function () {
-                    $scope.value = $element.html();
-                    if ($scope.prefix) {
-                        $scope.value = $scope.value.replace($scope.prefix, '');
+                    if (ctrl.initialized) {
+                        $scope.value = $element.html();
+                        if ($scope.prefix) {
+                            $scope.value = $scope.value.replace($scope.prefix, '');
+                        }
                     }
                 };
 
                 $scope.accept = function () {
-                    if ($scope.model instanceof model
+                    if (ctrl.initialized
+                        && $scope.model instanceof model
                         && $scope.property
                         && $scope.model.get($scope.property) !== $scope.value
                     ) {
@@ -88,65 +97,83 @@
                     }
                 };
                 $scope.cancel = function () {
-                    if ($scope.model instanceof model && $scope.property) {
+                    if (ctrl.initialized
+                        && $scope.model instanceof model
+                        && $scope.property) {
                         $scope.value = $scope.model.get($scope.property);
                     }
                 };
 
-                // WATCHERS
-
-                if ($scope.alwaysEdit !== true && $scope.alwaysEdit !== 'true') {
-                    $scope.$watch($scope.liveEditStatus, function (liveEdit) {
-                        $element.attr('contenteditable', liveEdit);
-                        if (liveEdit) {
-                            $element.addClass('liveEdit');
-                        } else {
-                            $element.removeClass('liveEdit');
-                        }
-                    });
-                } else {
-                    $element.attr('contenteditable', true);
-                }
-
                 $scope.$watch('ngModel', function (data) {
                     if (data instanceof model && !_.isEqual(data, $scope.model)) {
                         $scope.model = data;
+                        var unwatch = $scope.$watch('model.data', function () {
+                            unwatch(); // Remove this watch as soon as it's run once
+                            ctrl.init(); // Initialize only after there is a model to work with
+                        });
                     }
                 });
-                $scope.$watch('model.data.' + $scope.property, function (data) {
-                    $scope.value = data;
-                    ngModel.$render(); // if the value changes, show the new change (since rendering doesn't always happen)
-                });
 
-                // TRIGGERS
+                // Init() will have data rendered from the model rather than the element and allows for editing.
+                ctrl.init = function () {
+                    if (!ctrl.initialized) {
+                        ctrl.initialized = true;
 
-                // Update value on change, save value on blur
-                $element.on('focusout keyup change', function () {
-                    $scope.$apply($scope.read);
-                    if ($scope.autoSave !== false && $scope.autoSave !== 'false') {
-                        switch (event.type) {
-                            case 'focusout':
-                            case 'blur':
-                                $scope.$apply($scope.accept);
-                                break;
+                        // WATCHERS
+
+                        $scope.$watch('model.data.' + $scope.property, function (data) {
+                            $scope.value = data;
+                            ngModel.$render(); // if the value changes, show the new change (since rendering doesn't always happen)
+                        });
+
+                        if ($scope.alwaysEdit !== true && $scope.alwaysEdit !== 'true') {
+                            $scope.$watch($scope.liveEditStatus, function (liveEdit) {
+                                $element.attr('contenteditable', liveEdit);
+                                if (liveEdit) {
+                                    $element.addClass('liveEdit');
+                                } else {
+                                    $element.removeClass('liveEdit');
+                                }
+                            });
+                        } else {
+                            $element.attr('contenteditable', true);
                         }
-                    }
-                });
 
-                // Save / Cancel value on key press
-                $element.on('keydown keypress', function (event) {
-                    switch (event.which) {
-                        case Stratus.Key.Enter:
-                            event.preventDefault(); // Prevent Line breaks
-                            if ($scope.autoSave !== false && $scope.autoSave !== 'false') { // Placed here because we still want to prevent Line breaks
-                                $scope.$apply($scope.accept);
+                        // TRIGGERS
+
+                        // Update value on change, save value on blur
+                        $element.on('focusout keyup change', function () {
+                            if (ctrl.initialized) {
+                                $scope.$apply($scope.read);
+                                if ($scope.autoSave !== false && $scope.autoSave !== 'false') {
+                                    switch (event.type) {
+                                        case 'focusout':
+                                        case 'blur':
+                                            $scope.$apply($scope.accept);
+                                            break;
+                                    }
+                                }
                             }
-                            break;
-                        case Stratus.Key.Escape:
-                            $scope.$apply($scope.cancel);
-                            break;
+                        });
+
+                        // Save / Cancel value on key press
+                        $element.on('keydown keypress', function (event) {
+                            if (ctrl.initialized) {
+                                switch (event.which) {
+                                    case Stratus.Key.Enter:
+                                        event.preventDefault(); // Prevent Line breaks
+                                        if ($scope.autoSave !== false && $scope.autoSave !== 'false') { // Placed here because we still want to prevent Line breaks
+                                            $scope.$apply($scope.accept);
+                                        }
+                                        break;
+                                    case Stratus.Key.Escape:
+                                        $scope.$apply($scope.cancel);
+                                        break;
+                                }
+                            }
+                        });
                     }
-                });
+                };
             }
         };
     };
