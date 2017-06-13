7b19c03cfc34d391b4a652884972bd415df9c04f
Alex Gurrola <alexgurrola@gutensite.com>
2017-06-13 16:33:10 -0700

    Fix Attribute Setters / Pagination

--- components/pagination.js
+++ components/pagination.js
@@ -49,23 +49,23 @@
             });
 
             // Handle Page Changes
-            $scope.$watch('collection.meta.data.pageCurrent', function (pageCurrent) {
+            $scope.$watch('collection.meta.data.pagination.pageCurrent', function (pageCurrent) {
                 if (!pageCurrent) return true;
-                if ($scope.collection.meta.get('pageTotal') <= 10) {
+                if ($scope.collection.meta.get('pagination.pageTotal') <= 10) {
                     // less than 10 total pages so show all
                     $scope.startPage = 1;
-                    $scope.endPage = $scope.collection.meta.get('pageTotal');
+                    $scope.endPage = $scope.collection.meta.get('pagination.pageTotal');
                 } else {
                     // more than 10 total pages so calculate start and end pages
-                    if ($scope.collection.meta.get('pageCurrent') <= 6) {
+                    if ($scope.collection.meta.get('pagination.pageCurrent') <= 6) {
                         $scope.startPage = 1;
                         $scope.endPage = 10;
-                    } else if ($scope.collection.meta.get('pageCurrent') + 4 >= $scope.collection.meta.get('pageTotal')) {
-                        $scope.startPage = $scope.collection.meta.get('pageTotal') - 9;
-                        $scope.endPage = $scope.collection.meta.get('pageTotal');
+                    } else if ($scope.collection.meta.get('pagination.pageCurrent') + 4 >= $scope.collection.meta.get('pagination.pageTotal')) {
+                        $scope.startPage = $scope.collection.meta.get('pagination.pageTotal') - 9;
+                        $scope.endPage = $scope.collection.meta.get('pagination.pageTotal');
                     } else {
-                        $scope.startPage = $scope.collection.meta.get('pageCurrent') - 5;
-                        $scope.endPage = $scope.collection.meta.get('pageCurrent') + 4;
+                        $scope.startPage = $scope.collection.meta.get('pagination.pageCurrent') - 5;
+                        $scope.endPage = $scope.collection.meta.get('pagination.pageCurrent') + 4;
                     }
                 }
                 if (!isNaN($scope.startPage) && !isNaN($scope.endPage)) {
