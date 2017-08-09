fed9606f9b17ce86f04a3f1e368ef00fea349570
Alex Gurrola <alexgurrola@gutensite.com>
2017-08-09 14:30:09 -0700

    Minor Repairs

--- components/editLegacy.js
+++ components/editLegacy.js
@@ -1,27 +1,17 @@
-//     Stratus.Components.EditLegacy 1.0
-
-//     Copyright (c) 2017 by Sitetheory, All Rights Reserved
-//
-//     All information contained herein is, and remains the
-//     property of Sitetheory and its suppliers, if any.
-//     The intellectual and technical concepts contained herein
-//     are proprietary to Sitetheory and its suppliers and may be
-//     covered by U.S. and Foreign Patents, patents in process,
-//     and are protected by trade secret or copyright law.
-//     Dissemination of this information or reproduction of this
-//     material is strictly forbidden unless prior written
-//     permission is obtained from Sitetheory.
-//
-//     For full details and documentation:
-//     http://docs.sitetheory.io
-
-// Stratus Edit Legacy Component
+// Edit Component (Legacy)
 // ----------------------
 
 // Define AMD, Require.js, or Contextual Scope
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['stratus', 'underscore', 'angular', 'angular-material', 'stratus.services.model', 'froala'], factory);
+        define([
+            'stratus',
+            'underscore',
+            'angular',
+            'angular-material',
+            'stratus.services.model',
+            'froala'
+        ], factory);
     } else {
         factory(root.Stratus, root._);
     }
