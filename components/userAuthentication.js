33138061f0d93c3ab1804679f34b26adbc4f2286
Phuc Pham <phuc.pham@codeenginestudio.com>
2017-11-01 17:00:50 +0700

    add .less file and refactor code

--- components/userAuthentication.js
+++ components/userAuthentication.js
@@ -27,13 +27,9 @@
       Stratus.Instances[this.uid] = $scope;
       $scope.elementId = $attrs.elementId || this.uid;
 
-      // Check if Component is Available
-      $log.log('UserAuthentication Initialization', this.uid, $scope);
-
-      // Configure Controller
       var $ctrl = this;
       $ctrl.emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
-      $ctrl.passwordRegex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/;
+      $ctrl.passwordRegex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/i;
       $ctrl.enabledForgotPassForm = false;
       $ctrl.forgotPasstext = 'Forgot Password ?';
       $ctrl.showForgotPassForm = showForgotPassForm;
@@ -41,19 +37,26 @@
       $ctrl.doSignUp = doSignUp;
       $ctrl.doResetPass = doResetPass;
 
-      function showForgotPassForm() {
-        $ctrl.forgotPasstext = $ctrl.enabledForgotPassForm ? 'Forgot Password?' : 'Back to login';
-        $ctrl.enabledForgotPassForm = !$ctrl.enabledForgotPassForm;
+      function showForgotPassForm(isShow) {
+          $ctrl.forgotPasstext = isShow ? 'Back to login' : 'Forgot Password ?';
+          $ctrl.enabledForgotPassForm = isShow;
       }
 
-      // API/Login
+      //API/Login
       function doSignIn(signinData) {
-        $log.log('doSignIn', signinData);
+          console.log(signinData);
+          //
       }
 
-      // [POST]API/User
+      //[POST]API/User
       function doSignUp(signupData) {
-        $log.log('doSignUp', signupData.email, validatePhoneNumber(signupData.phone));
+          console.log(signupData.email);
+          console.log(validatePhoneNumber(signupData.phone));
+      }
+
+      function doResetPass(resetPassData) {
+          console.log(resetPassData.email);
+          console.log(resetPassData.phone);
       }
 
       function doResetPass(resetPassData) {
@@ -69,3 +72,4 @@
     templateUrl: Stratus.BaseUrl + 'sitetheorystratus/stratus/components/userAuthentication' + (Stratus.Environment.get('production') ? '.min' : '') + '.html'
   };
 }));
+
