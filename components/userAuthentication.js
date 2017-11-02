e9e91841fafc5f7276ead62168bb3a1e203e5b18
Phuc Pham <phuc.pham@codeenginestudio.com>
2017-11-02 14:43:18 +0700

    success call login api and sign-up api

--- components/userAuthentication.js
+++ components/userAuthentication.js
@@ -20,7 +20,7 @@
   // This component intends to allow editing of various selections depending on context.
   Stratus.Components.UserAuthentication = {
     bindings: {},
-    controller: function($scope, $attrs, $log, $http) {
+    controller: function($scope ,$window, $attrs, $log, $http) {
       // Initialize
       this.uid = _.uniqueId('user_authentication_');
       Stratus.Internals.CssLoader(Stratus.BaseUrl + 'sitetheorystratus/stratus/components/userAuthentication' + (Stratus.Environment.get('production') ? '.min' : '') + '.less');
@@ -28,41 +28,79 @@
       $scope.elementId = $attrs.elementId || this.uid;
 
       var $ctrl = this;
+      $ctrl.signInIndex = 0;
+      $ctrl.signUpIndex = 1;
       $ctrl.emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
       $ctrl.passwordRegex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/i;
       $ctrl.enabledForgotPassForm = false;
-      $ctrl.forgotPasstext = 'Forgot Password ?';
+      $ctrl.forgotPasstext = 'Forgot Password?';
       $ctrl.showForgotPassForm = showForgotPassForm;
       $ctrl.doSignIn = doSignIn;
       $ctrl.doSignUp = doSignUp;
       $ctrl.doResetPass = doResetPass;
+      $ctrl.onTabSelected = onTabSelected;
+      $ctrl.errorMsg = null;
+      $ctrl.selectedIndex = $ctrl.signInIndex;
       var url = '/Api/User';
+      var loginUrl = '/Api/Login';
 
       function showForgotPassForm(isShow) {
-          $ctrl.forgotPasstext = isShow ? 'Back to login' : 'Forgot Password ?';
+          $ctrl.forgotPasstext = isShow ? 'Back to login' : 'Forgot Password?';
           $ctrl.enabledForgotPassForm = isShow;
+          if(!isShow){
+            $ctrl.selectedIndex = $ctrl.signInIndex;
+          }
+      }
+
+      //Show Tab
+      function onTabSelected(index) {
+        $ctrl.selectedIndex = index;
+        $ctrl.errorMsg = null;
       }
 
       //API/Login
       function doSignIn(signinData) {
-          console.log(signinData);
-          //
+        $ctrl.errorMsg = null;
+        var data = {
+          'email': signinData.email,
+          'password': signinData.password
+        }
+
+        $http({ method: 'POST', url: loginUrl, data: data }).then(
+          function(response){
+            console.log('sign in ', response);
+            var code = getStatus(response).code;
+            var message = getStatus(response).message;
+            if(code == 'SUCCESS'){
+              return $window.location.href = '/';
+            }else {
+              $ctrl.errorMsg = message;
+            }
+          }
+        )
       }
 
       //[POST]API/User
       function doSignUp(signupData) {
-          var data = {
-              'email': signupData.email,
-              'phone': validatePhoneNumber(signupData.phone)
-          }
+        $ctrl.errorMsg = null;
+        var data = {
+          'email': signupData.email,
+          'phone': validatePhoneNumber(signupData.phone)
+        }
 
-          $http({ method: 'POST',  url: url,  data: data}).then(
-              function(response) {
-                  console.log(response);
-              }, function(error) {
-                  console.log(error);
-              });
-      }
+        $http({ method: 'POST',  url: url,  data: data}).then(
+          function(response) {
+            console.log(getStatus(response));
+            var code = getStatus(response).code;
+            var message = getStatus(response).message;
+
+            if(code == 'SUCCESS'){
+              return $window.location.href = '/';
+            }else {
+              $ctrl.errorMsg = message;
+            }
+          });
+        }
 
       function doResetPass(resetPassData) {
           var data = {
@@ -87,8 +125,11 @@
         var phoneRegex = /^[0-9().+-]*/g;
         return phone.match(phoneRegex)[0];
       };
+
+      var getStatus = function(response){
+        return response.data.meta.status['0'];
+      }
     },
     templateUrl: Stratus.BaseUrl + 'sitetheorystratus/stratus/components/userAuthentication' + (Stratus.Environment.get('production') ? '.min' : '') + '.html'
   };
 }));
-
