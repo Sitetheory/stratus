fa7917949b6a8bc31a9e7d553d87bd159a7189a8
Dung Pham <dung.pham@codeenginestudio.com>
2017-11-08 16:29:04 +0700

    update password progress bar

--- components/userAuthentication.js
+++ components/userAuthentication.js
@@ -1,4 +1,3 @@
-// Define AMD, Require.js, or Contextual Scope
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([
@@ -9,12 +8,13 @@
       'angular',
 
       // Modules
-      'angular-material'
+      'zxcvbn',
     ], factory);
   } else {
-    factory(root.Stratus, root._);
+    // Browser globals
+    factory(root.Stratus, root._, root.angular, root.zxcvbn);
   }
-}(this, function (Stratus, _) {
+}(typeof self !== 'undefined' ? self : this, function (Stratus, _, angular, zxcvbn) {
   // This component intends to allow editing of various selections depending on context.
   Stratus.Components.UserAuthentication = {
     controller: function ($scope, $window, $attrs, $log, $http, $mdDialog) {
@@ -29,14 +29,17 @@
       // variables
       var url = '/Api/User';
       var loginUrl = '/Api/Login';
+      $ctrl.signinData = {};
+      $ctrl.resetPassData = {};
+      $ctrl.allowSubmit = false;
       $ctrl.signInIndex = 0;
       $ctrl.signUpIndex = 1;
       $ctrl.emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/i;
-      $ctrl.passwordRegex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/;
       $ctrl.phoneRegex = /^[\d+\-()]+$/;
       $ctrl.enabledForgotPassForm = false;
       $ctrl.isHandlingUrl = getUrlParams().type !== null ? true : false;
       $ctrl.enabledResetPassForm = getUrlParams().type === 'reset-password' ? true : false;
+      $ctrl.resetPassHeaderEmail = getUrlParams().type === 'reset-password' ? getUrlParams().email : null;
       $ctrl.enabledVerifyForm = getUrlParams().type === 'verify' ? true : false;
       $ctrl.forgotPassText = 'Forgot Password?';
       $ctrl.resetPassHeaderText = 'Reset your account password';
@@ -53,6 +56,33 @@
       $ctrl.onTabSelected = onTabSelected;
       $ctrl.backToLogin = backToLogin;
       $ctrl.verifyAccount = verifyAccount;
+
+      // Watcher
+      $scope.$watch(angular.bind(this, function () {
+        if (!_.isEmpty(this.signinData)) {
+          return this.signinData.password;
+        }
+        if (!_.isEmpty(this.resetPassData)) {
+          return this.resetPassData.password;
+        }
+      }), function (newValue, oldValue) {
+        if (newValue !== undefined && newValue !== oldValue) {
+          $ctrl.progressBarClass = null;
+          $ctrl.progressBarValue = null;
+
+          generateProgressBar(newValue);
+
+          if (!validPassword(newValue)) {
+            $ctrl.errorMsg = 'Password must contain at least 8 characters, 1 lower and uppercase letter and 1 number.';
+          } else if ($ctrl.progressBarValue <= 40) {
+            $ctrl.errorMsg = 'Your password is not strong.';
+            $ctrl.allowSubmit = true;
+          } else {
+            $ctrl.errorMsg = '';
+            $ctrl.allowSubmit = true;
+          }
+        }
+      });
 
       // Define functional methods
       function showForgotPassForm(isShow) {
@@ -199,7 +229,7 @@
           var message = getStatus(response).message;
           if (code == 'SUCCESS') {
             $ctrl.isHandlingUrl = false;
-            return $window.location.href = '/';
+            return $window.location.href = '/Member/Sign-In';
           } else {
             $ctrl.errorMsg = message;
           }
@@ -207,6 +237,12 @@
           console.log(error);
         });
       }
+
+      // Helpers
+      function validPassword(password) {
+        var passwordRegex = /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/;
+        return password.length >= 8 && passwordRegex.test(password);
+      };
 
       function cleanedPhoneNumber(phone) {
         var keepNumberOnlyRegex = /\D+/g;
@@ -224,6 +260,31 @@
 
       function getStatus(response) {
         return response.data.meta.status['0'];
+      }
+
+      function generateProgressBar(password) {
+        switch (zxcvbn(password).score) {
+          case 0:
+            $ctrl.progressBarClass = 'risky';
+            $ctrl.progressBarValue = 20;
+            break;
+          case 1:
+            $ctrl.progressBarClass = 'guessable';
+            $ctrl.progressBarValue = 40;
+            break;
+          case 2:
+            $ctrl.progressBarClass = 'safely';
+            $ctrl.progressBarValue = 60;
+            break;
+          case 3:
+            $ctrl.progressBarClass = 'moderate';
+            $ctrl.progressBarValue = 80;
+            break;
+          case 4:
+            $ctrl.progressBarClass = 'strong';
+            $ctrl.progressBarValue = 100;
+            break;
+        }
       }
     },
     templateUrl: Stratus.BaseUrl + 'sitetheorystratus/stratus/components/userAuthentication' + (Stratus.Environment.get('production') ? '.min' : '') + '.html'
