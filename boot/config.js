75477ec4654884ec98569e78fe249a158aa15911
Dung Pham <dung.pham@codeenginestudio.com>
2017-11-14 16:27:49 +0700

    create a password_reset component for reset pass after login

--- boot/config.js
+++ boot/config.js
@@ -144,6 +144,7 @@ boot.config({
     'stratus.components.upload': boot.bundle + 'stratus/components/upload' + boot.suffix,
     'stratus.components.visualSelector': boot.bundle + 'stratus/components/visualSelector' + boot.suffix,
     'stratus.components.userAuthentication': boot.bundle + 'stratus/components/userAuthentication' + boot.suffix,
+    'stratus.components.passwordReset': boot.bundle + 'stratus/components/passwordReset' + boot.suffix,
 
     // TODO: Move these to Sitetheory since they are specific to Sitetheory
     'stratus.components.permission': boot.bundle + 'stratus/components/permission' + boot.suffix,
