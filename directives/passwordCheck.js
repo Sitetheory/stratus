3c5b86039975880000e8c45447f1ea0d662b9aa1
Dung Pham <dung.pham@codeenginestudio.com>
2017-11-27 17:11:04 +0700

    check for password validation after

---
+++ directives/passwordCheck.js
@@ -0,0 +1,30 @@
+// PasswordCheck Directive
+// ---------------------
+
+// Define AMD, Require.js, or Contextual Scope
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['stratus', 'underscore', 'angular'], factory);
+  } else {
+    factory(root.Stratus, root._);
+  }
+}(this, function (Stratus, _) {
+
+  // This directive intends to handle binding of a dynamic variable to
+  Stratus.Directives.PasswordCheck = function () {
+    return {
+      require: 'ngModel',
+      scope: {
+        password: '@'
+      },
+      link: function (scope, element, attr, ngModel) {
+        element.focusout(function () {
+          ngModel.$validators.passwordCheck = function (modelValue) {
+            return modelValue == scope.password;
+          };
+          ngModel.$validate();
+        });
+      }
+    };
+  };
+}));
