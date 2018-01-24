777798542e32a3f3ebbf3334618405b08b9528f7
Phuc Pham <phuc.pham@codeenginestudio.com>
2018-01-24 17:30:11 +0700

    add invoice product controller

--- boot/config.js
+++ boot/config.js
@@ -129,6 +129,7 @@ boot.config({
     'stratus.controllers.selectMainRoute': boot.bundle + 'stratus/controllers/selectMainRoute' + boot.suffix,
     'stratus.controllers.utilities': boot.bundle + 'stratus/controllers/utilities' + boot.suffix,
     'stratus.controllers.productFilter': boot.bundle + 'stratus/controllers/productFilter' + boot.suffix,
+    'stratus.controllers.invoiceProductFilter': boot.bundle + 'stratus/controllers/invoiceProductFilter' + boot.suffix,
 
     /* Stratus Core Components */
     'stratus.components.base': boot.bundle + 'stratus/components/base' + boot.suffix,
