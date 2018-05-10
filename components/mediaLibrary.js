4c0956444c3e6a0479a7608b0e61567bb3e9472b
Cuong Nguyen <cuong.nguyen@codeenginestudio.com>
2018-05-10 15:15:41 +0700

    fixed coding convension

--- components/mediaLibrary.js
+++ components/mediaLibrary.js
@@ -103,9 +103,9 @@
 
       function getYouTubeID (url) {
         var ID = ''
-        url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
+        url = url.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)
         if (url[2] !== undefined) {
-          ID = url[2].split(/[^0-9a-z_\-]/i)
+          ID = url[2].split(/[^0-9a-z_]/i)
           ID = ID[0]
         } else {
           ID = url
