574901b63a567a6278067cc588e4bd6957bdb70f
Nagulapalle, Rajender <rnagulapalle@paypal.com>
2018-09-18 21:36:53 -0700

    addressed Alex comments

--- services/collection.js
+++ services/collection.js
@@ -253,6 +253,14 @@
                   $log.log('request:', request)
                 }
                 request.then(function (models) {
+                  if (!Stratus.Environment.get('production')) {
+                    // TODO: Finish handling throttled data
+                    /* *
+                    $log.log('throttled:', _.map(models, function (model) {
+                      return model.domainPrimary
+                    }))
+                    /* */
+                  }
                   resolve(models)
                 }).catch(reject)
               })
@@ -330,6 +338,39 @@
                   return memo || angular.isDefined(data)
                 })
             }
+
+            // Infinite Scrolling
+            /* *
+            this.infiniteModels = {
+              numLoaded_: 0,
+              toLoad_: 0,
+              // Required.
+              getItemAtIndex: function (index) {
+                if (index > this.numLoaded_) {
+                  this.fetchMoreItems_(index)
+                  return null
+                }
+                return index
+              },
+              // Required.
+              // For infinite scroll behavior, we always return a slightly higher
+              // number than the previously loaded items.
+              getLength: function () {
+                return this.numLoaded_ + 5
+              },
+              fetchMoreItems_: function (index) {
+                // For demo purposes, we simulate loading more items with a timed
+                // promise. In real code, this function would likely contain an
+                // $http request.
+                if (this.toLoad_ < index) {
+                  this.toLoad_ += 20
+                  $timeout(angular.noop, 300).then(angular.bind(this, function () {
+                    this.numLoaded_ = this.toLoad_
+                  }))
+                }
+              }
+            }
+            /* */
           }
         }
       ])
