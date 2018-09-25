ebfec4f74da26fa371500224cffe8517a6385747
alexgurrola <alexgurrola1@gmail.com>
2018-09-24 18:36:21 -0700

    Fix OnScreen for Scroll Resets & Add Throttling

--- directives/onScreen.js
+++ directives/onScreen.js
@@ -31,7 +31,8 @@
         offScreen: '@offScreen',
         spy: '@spy',
         target: '@target',
-        scrollChange: '@scrollChange',
+        partial: '@partial', // by default: true
+        update: '@update', // by default: 100 ms
         event: '@event', // event can be multiple listeners: reset
         offset: '@offset', // The distance the spy element is allowed to enter the screen before triggering 'onscreen'
         reset: '@reset' // The location on the page that should trigger a reset (removal of all classes). Defaults to 0 (top of page)
@@ -42,42 +43,73 @@
         scope.elementId = attrs.elementId || element.uid
 
         // event can be multiple listeners: reset
-        const event = attrs.event ? attrs.event.split(' ') : []
-        const target = attrs.target ? $(attrs.target) : element
-        let spy = attrs.spy ? $(attrs.spy) : element
+        let $element = element instanceof $ ? element : $(element)
+        let event = attrs.event ? attrs.event.split(' ') : []
+        let target = attrs.target ? $(attrs.target) : $element
+        let spy = attrs.spy ? $(attrs.spy) : $element
         if (!spy.length) {
-          spy = element
+          spy = $element
         }
+        let partial = _.hydrate(attrs.partial)
+        if (typeof partial !== 'boolean') {
+          partial = true
+        }
+        let update = _.hydrate(attrs.update)
+        if (typeof update !== 'number') {
+          update = 100
+        }
+        let lastUpdate = 0
+        let isWaiting = false
+        let wasOnScreen = false
 
         // The distance the spy element is allowed to enter the screen before triggering 'onscreen'
-        const offset = _.hydrate(attrs.offset || 0)
+        let offset = _.hydrate(attrs.offset) || 0
 
         // The location on the page that should trigger a reset (removal of all classes). Defaults to 0 (top of page)
-        const reset = attrs.reset || 0
+        let reset = _.hydrate(attrs.reset) || 0
 
         // Custom Methods for On/Off Screen
         // TODO: Add Parsing Here
-        const onScreen = function () {
+        let onScreen = function () {
           return attrs.onScreen && typeof attrs.onScreen === 'function' ? attrs.onScreen() : true
         }
-        const offScreen = function () {
+        let offScreen = function () {
           return attrs.offScreen && typeof attrs.offScreen === 'function' ? attrs.offScreen() : true
         }
+        let isOnScreen = function () {
+          if (isWaiting) {
+            return
+          }
+          let calculation = (new Date()).getTime()
+          if (calculation - lastUpdate > update) {
+            lastUpdate = calculation
+            wasOnScreen = Stratus.Internals.IsOnScreen(spy, offset, partial)
+          } else {
+            isWaiting = true
+            setTimeout(function () {
+              isWaiting = false
+              calculate()
+            }, ((lastUpdate + update) - calculation) + 1)
+          }
+          return wasOnScreen
+        }
 
-        // Ensure OnScroll is listening
-        Stratus.Internals.OnScroll()
-
-        const isOnScreen = function () {
-          if (Stratus.Internals.IsOnScreen(spy, offset)) { // TODO: Move this part to a function so it can be either on scroll change or constant
+        // Bind Angular to Environment
+        let calculate = function () {
+          // remove all classes when the scroll is all the way back at the top of the page (or the spy is above a specific location specified location)
+          if (event.indexOf('reset') !== -1 && ((reset > 0 && $element.offset().top <= reset) || $(window).scrollTop() <= 0)) {
+            target.removeClass('onScreen offScreen scrollUp scrollDown reveal conceal')
+            target.addClass('reset')
+            return
+          }
+          if (isOnScreen()) {
             // Add init class so we can know it's been on screen before
             if (!target.hasClass('onScreen')) {
               target.addClass('onScreen onScreenInit')
             }
-
             if (target.hasClass('offScreen')) {
               target.removeClass('offScreen')
             }
-
             // Execute Custom Methods
             onScreen()
           } else {
@@ -87,33 +119,20 @@
             if (!target.hasClass('offScreen')) {
               target.addClass('offScreen')
             }
-
             // Execute Custom Methods
             offScreen()
           }
         }
 
-        // Bind Angular to Environment
-        Stratus.Environment.on('change:viewPortChange', function (model) {
-          // Reset
-          let isReset = false
-          if (event.indexOf('reset') !== -1) {
-            // remove all classes when the scroll is all the way back at the top of the page (or the spy is above a specific location specified location)
-            if ((reset > 0 && $(element).offset().top <= reset) || $(window).scrollTop() <= 0) {
-              isReset = true
-              target.removeClass('onScreen offScreen scrollUp scrollDown ascend descend')
-            }
-          }
-          if (!isReset && !_.hydrate(attrs.scrollChange)) {
-            isOnScreen()
-          }
-        })
-        Stratus.Environment.on('change:lastScroll', function (model) {
+        // Ensure OnScroll is listening
+        Stratus.Internals.OnScroll()
+        Stratus.Environment.on('change:viewPortChange', calculate)
+        Stratus.Environment.on('change:lastScroll', function () {
           // If no scrolling has occurred remain false
           let lastScroll = Stratus.Environment.get('lastScroll')
 
           // Add scroll classes no matter what, so you can target styles when the item is on or off screen depending on scroll action
-          if (lastScroll === 'down') {
+          if (lastScroll === 'down' && !target.hasClass('reset')) {
             if (!target.hasClass('scrollDown')) {
               target.addClass('scrollDown')
             }
@@ -129,21 +148,10 @@
             if (target.hasClass('scrollDown')) {
               target.removeClass('scrollDown')
             }
+            if (target.hasClass('reset')) {
+              target.removeClass('reset')
+            }
           }
-
-          if (_.hydrate(attrs.scrollChange)) {
-            isOnScreen()
-          }
-
-          /* *
-          console.log('OnScreen:', {
-            direction: lastScroll,
-            spy: spy,
-            reset: reset,
-            isReset: isReset,
-            offset: offset
-          }, Stratus.Internals.IsOnScreen(spy, offset))
-          /* */
         })
       }
     }
