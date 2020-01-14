ff1f76d12bc341addc4a2709fa1385e747c0b85f
Matthew Davis <apocist@yahoo.com>
2020-01-14 11:38:04 -0800

    @stratusjs/idx ListTrac enabled to chosen MLS services

--- packages/idx/src/listTrac.ts
+++ packages/idx/src/listTrac.ts
@@ -12,7 +12,7 @@ import {DOMComplete} from '@stratusjs/core/dom'
 // Services
 import '@stratusjs/angularjs/services/model' // Needed as $provider
 // import {Model} from '@stratusjs/angularjs/services/model' // Needed as Class
-import '@stratusjs/angularjs/services/collection' // Needed as $provider
+import '@stratusjs/angularjs/services/collection'
 // import {Collection} from '@stratusjs/angularjs/services/collection' // Needed as Class
 
 // Stratus Dependencies
@@ -31,12 +31,18 @@ Stratus.Services.ListTrac = [
         $provide.factory('ListTrac', (
             $injector: angular.auto.IInjectorService,
             $http: angular.IHttpService,
-            $q: angular.IQService
+            $q: angular.IQService,
+            $window: angular.IWindowService,
             ) => {
-                console.log('Running ListTrac')
+                console.log('ListTrac service initting')
+                const eventQueue: Array<() => void> = []
                 let listTracLoaded = false
                 let listTracPending = false
                 let listTracAccountId = ''
+                let LT: any
+                let eventTypes: {
+                    [key: string]: number
+                } = {}
                 /*import('https://code.listtrac.com/monitor.ashx?acct=x_100633&nonjq=1').then(test => {
                     console.log('imported')
                 })*/
@@ -50,43 +56,102 @@ Stratus.Services.ListTrac = [
 
                 async function loadListTrac() {
                     if (!listTracAccountId) {
-                        console.warn('ListTrac Account not provided, will not load')
+                        console.warn('ListTrac Account Id not provided, will not load')
                         return
                     }
                     // Only attempt to load ListTrac if it's not already loaded or currently attempting to load
                     if (!listTracPending && !listTracLoaded) {
                         try {
                             listTracPending = true
-                            // TODO check if already loaded
                             await DOMComplete()
-                            await require(`https://code.listtrac.com/monitor.ashx?nonjq=1&acct=${listTracAccountId}`)
-                            listTracLoaded = true
-                            console.log('imported ListTrac')
+                            // console.log('importing ListTrac')
+                            // Inject the javascript file into the HEAD to load
+                            const jsFile: HTMLScriptElement = document.createElement('script')
+                            jsFile.type = 'application/javascript'
+                            jsFile.async = true
+                            jsFile.src = `https://code.listtrac.com/monitor.ashx?nonjq=1&acct=${listTracAccountId}`
+                            jsFile.onload = () => loadedListTrac()
+                            Stratus.Select('head').append(jsFile)
+
+                            // TODO make a timeout to check if it still doesn't load after some to time manually check..?
                         } catch (e) {
                             console.error(e)
                         }
-                        listTracPending = false
                     }
+                }
+
+                async function loadedListTrac() {
+                    if (
+                        Object.prototype.hasOwnProperty.call($window, '_LT') &&
+                        typeof ($window._LT) !== 'undefined' &&
+                        $window._LT !== null
+                    ) {
+                        eventTypes = $window._eventType
+                        LT = $window._LT
+                        listTracLoaded = true
+                        listTracPending = false
+                        processEventQueue()
+                    } else {
+                        listTracLoaded = false
+                        console.warn('ListTrac failed to load')
+                    }
+                    listTracPending = false
                 }
 
                 function setAccountId(accountId: string): void {
                     listTracAccountId = accountId
-                    console.log('Set account id to', listTracAccountId)
-                    loadListTrac() // TODO should only load when a trackEvent needs to happen (e.g. Details view)
+                    // console.log('Set account id to', listTracAccountId)
                 }
 
-                function randomFunction(): string {
-                    return 'Test Function Ran'
+                async function track(eventName: string, mlsId: string, postalCode: number, agentId: string): Promise<void> {
+                    // console.log('setting up Event', eventName)
+                    await loadListTrac()
+                    const promiseFunction = () => {
+                        /*
+                        console.log('will run LT._trackEvent(',
+                            eventTypes[eventName],
+                            mlsId,
+                            postalCode,
+                            agentId,
+                            ')'
+                        )
+                         */
+
+                        LT._trackEvent(
+                            eventTypes[eventName],
+                            mlsId,
+                            postalCode,
+                            agentId
+                        )
+
+                        return
+                    }
+                    addEventToQueue(promiseFunction)
                 }
 
-                async function randomFunctionAfterDOM(): Promise<string> {
-                    await DOMComplete()
-                    return 'Test Function Ran After DOM'
+                function addEventToQueue(event: () => void) {
+                    // console.log('addEventToQueue added')
+                    eventQueue.push(event)
+                    if (listTracLoaded) {
+                        processEventQueue()
+                    }
+                }
+
+                function processEventQueue() {
+                    // console.log('processEventQueue running')
+                    if (!listTracLoaded) {
+                        console.warn('Cannot processEventQueue as ListTrac not loaded')
+                        return
+                    }
+                    while (eventQueue.length > 0) {
+                        // $q(eventQueue.shift()).then(() => console.log('Promise done'))
+                        $q(eventQueue.shift())
+                    }
                 }
 
                 return {
-                    randomFunction,
-                    setAccountId
+                    setAccountId,
+                    track
                 }
             }
         )
