310f767f40b0b693e04b99a1ae6cbfb67ebb927d
alexgurrola <alexgurrola1@gmail.com>
2020-06-25 18:51:39 -0700

    Add SA-Editor Component (WIP)
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

---
+++ packages/angular/src/editor/editor.component.ts
@@ -0,0 +1,296 @@
+// Angular Core
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    ElementRef,
+    Input,
+    // OnChanges,
+    // OnInit,
+    // Output,
+    // SecurityContext
+} from '@angular/core'
+import {FormControl} from '@angular/forms'
+
+// CDK
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop'
+
+// External
+import {Observable, Subject, Subscriber} from 'rxjs'
+// import {map, startWith} from 'rxjs/operators'
+
+// SVG Icons
+import {DomSanitizer, ɵDomSanitizerImpl} from '@angular/platform-browser'
+import {MatIconRegistry} from '@angular/material/icon'
+
+// RXJS
+import {SubjectSubscriber} from 'rxjs/internal/Subject'
+
+// External Dependencies
+import {Stratus} from '@stratusjs/runtime/stratus'
+import _ from 'lodash'
+import {keys} from 'ts-transformer-keys'
+
+// Components
+import {RootComponent} from '@stratusjs/angular/core/root.component'
+
+// Services
+import {Registry} from '@stratusjs/angularjs/services/registry'
+import {cookie} from '@stratusjs/core/environment'
+
+// Core Classes
+import {EventManager} from '@stratusjs/core/events/eventManager'
+import {ModelBase} from '@stratusjs/core/datastore/modelBase'
+import {EventBase} from '@stratusjs/core/events/eventBase'
+
+// AngularJS Classes
+import {Model} from '@stratusjs/angularjs/services/model'
+import {Collection} from '@stratusjs/angularjs/services/collection'
+
+// Local Setup
+const localDir = `/assets/1/0/bundles/${boot.configuration.paths['@stratusjs/angular/*'].replace(/[^/]*$/, '')}`
+const systemDir = '@stratusjs/angular'
+const moduleName = 'editor'
+
+const has = (object: object, path: string) => _.has(object, path) && !_.isEmpty(_.get(object, path))
+
+// export interface Model {
+//     completed: boolean;
+//     data: object;
+// }
+
+/**
+ * @title AutoComplete Selector with Drag&Drop Sorting
+ */
+@Component({
+    // selector: 'sa-selector-component',
+    selector: `sa-${moduleName}`,
+    templateUrl: `${localDir}/${moduleName}/${moduleName}.component.html`,
+    // FIXME: This doesn't work, as it seems Angular attempts to use a System.js import instead of their own, so it will
+    // require the steal-css module
+    // styleUrls: [
+    //     `${localDir}/${moduleName}/${moduleName}.component.css`
+    // ],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class EditorComponent extends RootComponent { // implements OnInit, OnChanges
+
+    // Basic Component Settings
+    title = moduleName + '_component'
+    uid: string
+
+    // Registry Attributes
+    @Input() target: string
+    @Input() targetSuffix: string
+    @Input() id: number
+    @Input() manifest: boolean
+    @Input() decouple: boolean
+    @Input() direct: boolean
+    @Input() api: object
+    @Input() urlRoot: string
+
+    // Component Attributes
+    @Input() property: string
+
+    // Dependencies
+    _ = _
+    has = has
+    log = console.log
+
+    // Stratus Data Connectivity
+    registry = new Registry()
+    fetched: Promise<boolean|Collection|Model>
+    data: any
+    collection?: EventBase
+    // @Output() model: any;
+    model?: ModelBase
+
+    // Observable Connection
+    dataSub: Observable<[]>
+    onChange = new Subject()
+    subscriber: Subscriber<any>
+    // Note: It may be better to LifeCycle::tick(), but this works for now
+
+    // Icon Localization
+    svgIcons: {
+        [key: string]: string
+    } = {}
+
+    // UI Flags
+    styled = false
+
+    constructor(
+        private iconRegistry: MatIconRegistry,
+        private sanitizer: DomSanitizer,
+        private ref: ChangeDetectorRef,
+        private elementRef: ElementRef
+    ) {
+        // Chain constructor
+        super()
+
+        // Initialization
+        this.uid = _.uniqueId(`sa_${moduleName}_component_`)
+        Stratus.Instances[this.uid] = this
+
+        // SVG Icons
+        // iconRegistry.addSvgIcon(
+        //     'selector:delete',
+        //     sanitizer.bypassSecurityTrustResourceUrl('/Api/Resource?path=@SitetheoryCoreBundle:images/icons/actionButtons/delete.svg')
+        // )
+
+        // TODO: Assess & Possibly Remove when the System.js ecosystem is complete
+        // Load Component CSS until System.js can import CSS properly.
+        Stratus.Internals.CssLoader(`${localDir}${moduleName}/${moduleName}.component.css`)
+            .then(() => {
+                this.styled = true
+                this.refresh()
+            })
+            .catch(() => {
+                console.error('CSS Failed to load for Component:', this)
+                this.styled = true
+                this.refresh()
+            })
+
+        // TODO: Allow more CSS files to get pulled and mark this.styled appropriately
+        if (_.has(boot.configuration.paths, 'quill')) {
+            const quillDir = `/assets/1/0/bundles/${boot.configuration.paths.quill.replace(/[^/]*$/, '')}`
+            Stratus.Internals.CssLoader(`${quillDir}quill.core.css`)
+            // Stratus.Internals.CssLoader(`${quillDir}quill.bubble.css`)
+            Stratus.Internals.CssLoader(`${quillDir}quill.snow.css`)
+        }
+
+        // Hydrate Root App Inputs
+        this.hydrate(elementRef, sanitizer, keys<EditorComponent>())
+
+        // Data Connections
+        this.fetchData()
+            .then(data => {
+                if (!data || !(data instanceof EventManager)) {
+                    console.warn('Unable to bind data from Registry!')
+                    return
+                }
+                // Manually render upon model change
+                // this.ref.detach();
+                const onDataChange = () => {
+                    if (!data.completed) {
+                        return
+                    }
+                    // this.onDataChange();
+                    this.dataDefer(this.subscriber)
+                    this.refresh()
+                }
+                data.on('change', onDataChange)
+                onDataChange()
+            })
+
+        // Declare Observable with Subscriber (Only Happens Once)
+        this.dataSub = new Observable(subscriber => this.dataDefer(subscriber))
+
+        // console.info('constructor!');
+    }
+
+    // ngOnInit() {
+    //     console.info('selector.ngOnInit')
+    // }
+
+    // ngOnChanges() {
+    //     // Display Inputs
+    //     if (!cookie('env')) {
+    //         return
+    //     }
+    //     console.log('inputs:', {
+    //         target: this.target,
+    //         targetSuffix: this.targetSuffix,
+    //         id: this.id,
+    //         manifest: this.manifest,
+    //         decouple: this.decouple,
+    //         direct: this.direct,
+    //         api: this.api,
+    //         urlRoot: this.urlRoot,
+    //     })
+    // }
+
+    // ngDoCheck(): void {
+    //     console.info('ngDoCheck:', this.dataSub);
+    // }
+
+    public refresh() {
+        if (!this.ref) {
+            console.error('ref not available:', this)
+            return
+        }
+        this.ref.detach()
+        this.ref.detectChanges()
+        this.ref.reattach()
+    }
+
+    // Data Connections
+    fetchData() {
+        if (this.fetched) {
+            return this.fetched
+        }
+        return this.fetched = this.registry.fetch(
+            Stratus.Select(this.elementRef.nativeElement),
+            this
+        )
+        // return this.fetched = this.registry.fetch({
+        //     target: this.target,
+        //     targetSuffix: this.targetSuffix,
+        //     id: this.id,
+        //     manifest: this.manifest,
+        //     decouple: this.decouple,
+        //     direct: this.direct,
+        //     api: this.api,
+        //     urlRoot: this.urlRoot,
+        // }, this)
+    }
+
+    // Ensures Data is populated before hitting the Subscriber
+    dataDefer(subscriber: Subscriber<any>) {
+        this.subscriber = this.subscriber || subscriber
+        if (!this.subscriber) {
+            return
+        }
+        const model = this.dataRef()
+        if (!model) {
+            setTimeout(() => {
+                this.dataDefer(subscriber)
+            }, 500)
+            return
+        }
+        // console.log('pushed model to subscriber.')
+        this.subscriber.next(model)
+        // TODO: Add a returned Promise to ensure async/await can use this defer directly.
+    }
+
+    dataRef(): string {
+        if (!this.model) {
+            return ''
+        }
+        const data = this.model.get(this.property)
+        if (!data) {
+            return ''
+        }
+        if (!_.isString(data)) {
+            return ''
+        }
+        return data
+    }
+
+    // selectedModel (observer: any) : any {
+    //     if (!this.data) {
+    //         this.fetchData().then(function (data: any) {
+    //             observer.next(data)
+    //         });
+    //     }
+    //     // data.on('change', () => observer.next(that.dataRef()));
+    //     observer.next()
+    // }
+
+    onDataChange() {
+        // FIXME: This is not in use due to contextual issues.
+        this.dataDefer(this.subscriber)
+        this.refresh()
+    }
+}
