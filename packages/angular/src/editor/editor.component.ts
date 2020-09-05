75c3fdbd526dce4df7b784030773362e27fc1f9a
alexgurrola <alexgurrola1@gmail.com>
2020-09-04 17:56:04 -0700

    New Angular-Editor
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

--- packages/angular/src/editor/editor.component.ts
+++ packages/angular/src/editor/editor.component.ts
@@ -45,6 +45,11 @@ import {
 } from '@stratusjs/runtime/stratus'
 import _ from 'lodash'
 
+// Angular Editor Dependencies
+import {
+    AngularEditorConfig
+} from '@kolkov/angular-editor'
+
 // Quill Dependencies
 import Quill from 'quill'
 import {
@@ -184,6 +189,55 @@ export class EditorComponent extends RootComponent implements OnInit, TriggerInt
 
     // Child Components
     quill: Quill
+    editorConfig: AngularEditorConfig = {
+        editable: true,
+        spellcheck: true,
+        height: 'auto',
+        minHeight: '0',
+        maxHeight: 'auto',
+        width: 'auto',
+        minWidth: '0',
+        translate: 'yes',
+        enableToolbar: true,
+        showToolbar: true,
+        placeholder: 'Enter text here...',
+        defaultParagraphSeparator: '',
+        defaultFontName: '',
+        defaultFontSize: '',
+        fonts: [
+            {class: 'arial', name: 'Arial'},
+            {class: 'times-new-roman', name: 'Times New Roman'},
+            {class: 'calibri', name: 'Calibri'},
+            {class: 'comic-sans-ms', name: 'Comic Sans MS'}
+        ],
+        customClasses: [
+            {
+                name: 'quote',
+                class: 'quote',
+            },
+            {
+                name: 'redText',
+                class: 'redText'
+            },
+            {
+                name: 'titleText',
+                class: 'titleText',
+                tag: 'h1',
+            },
+        ],
+        uploadUrl: `https://app.sitetheory.io:3000/?session=${cookie('SITETHEORY')}`,
+        uploadWithCredentials: false,
+        sanitize: false,
+        toolbarPosition: 'top',
+        toolbarHiddenButtons: [
+            [
+                'undo',
+                'redo'
+            ], [
+                // nothing yet
+            ]
+        ],
+    }
 
     constructor(
         // private iconRegistry: MatIconRegistry,
