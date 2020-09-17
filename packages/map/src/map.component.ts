2991ff21d9b95f4dd0caedf64c70510106c61773
Davis <apocist@yahoo.com>
2020-09-17 15:10:55 -0700

    @stratusjs/map fix loading icon

--- packages/map/src/map.component.ts
+++ packages/map/src/map.component.ts
@@ -289,6 +289,7 @@ export class MapComponent extends RootComponent implements OnInit, AfterViewInit
 
     /** Loads when this.map renders */
     async ngAfterViewInit() {
+        console.log('running ngAfterViewInit')
         try {
             await this.initGoogleMapsApi()
             this.map = new google.maps.Map(this.gMap.nativeElement, this.options)
@@ -303,6 +304,7 @@ export class MapComponent extends RootComponent implements OnInit, AfterViewInit
             console.error(this.uid, 'could not Init')
         }
         this.initializing = false
+        console.log('ngAfterViewInit done')
     }
 
     /**
