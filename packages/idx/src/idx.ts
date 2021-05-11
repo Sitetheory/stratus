0ef7e75b78a755a306e678556c6620d399ab7af3
Davis <apocist@yahoo.com>
2021-05-11 12:19:18 -0700

    @stratusjs/idx/property/search fixes Incorrectly Displaying in Url Without City/Location

--- packages/idx/src/idx.ts
+++ packages/idx/src/idx.ts
@@ -581,6 +581,8 @@ const angularJsService = (
     }
     // Blank options to initialize arrays
     const defaultWhereOptions: WhereOptions = {
+        City: '', // Added as default so search and manipulate
+        Location: '', // Added as default so search and manipulate
         Status: [],
         ListingType: [],
         CountyOrParish: [],
