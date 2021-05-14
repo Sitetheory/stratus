8e41f1cb14ed004f0807b74496a7af2d1dab1fa4
alexgurrola <alexgurrola1@gmail.com>
2021-05-13 22:46:15 -0700

    Angular 0.2.72: Editor & Selector Improvements
    Adds:
    
    - Editor: Code Mirror Support
    
    Changes:
    
    - Editor: `useClasses=false`
    
    Fixes:
    
    - Editor: Style Removal Bug
    - Selector: Fallbacks for `bestIdentifier`
    - Selector: Image Lazy Loading Display
    
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

--- packages/angular/src/editor/editor.component.ts
+++ packages/angular/src/editor/editor.component.ts
@@ -104,7 +104,8 @@ import {
 import {LooseObject} from '@stratusjs/core/misc'
 
 // Froala External Requirements (Before Plugins are Loaded)
-import 'codemirror'
+// @ts-ignore
+import * as CodeMirror from 'codemirror/lib/codemirror'
 import 'codemirror/mode/xml/xml'
 import 'html2pdf'
 import 'font-awesome'
@@ -456,11 +457,7 @@ export class EditorComponent extends RootComponent implements OnInit, TriggerInt
             indent_size: 4,
             wrap_line_length: 0
         },
-        // FIXME: The CodeView Plugin gives the following error when initializing CodeMirror.
-        // FIXME: TypeError: c.opts.codeMirror.fromTextArea is not a function
-        // TODO: Check if this is a timing issue
-        // TODO: https://froala.com/wysiwyg-editor/examples/code-mirror/
-        codeMirror: false,
+        codeMirror: CodeMirror,
         codeMirrorOptions: {
             indentWithTabs: false,
             lineNumbers: true,
@@ -583,9 +580,45 @@ export class EditorComponent extends RootComponent implements OnInit, TriggerInt
             'symbol', 'text', 'textPath', 'tspan', 'unknown', 'use', 'view',
         ],
         htmlAllowedStyleProps: [
-            '.+',
-            // 'font-family', 'font-size', 'background', 'color', 'width',
-            // 'text-align', 'vertical-align', 'background-color'
+            '.*'
+            // 'align-content', 'align-items', 'align-self', 'animation', 'animation-delay', 'animation-direction', 'animation-duration',
+            // 'animation-fill-mode', 'animation-iteration-count', 'animation-name', '@keyframes', 'animation-play-state',
+            // 'animation-timing-function',
+            // 'backface-visibility', 'background', 'background-attachment', 'background-clip', 'background-color', 'background-image',
+            // 'background-origin', 'background-position', 'background-repeat', 'background-size', 'border', 'border-bottom',
+            // 'border-bottom-color', 'border-bottom-left-radius', 'border-bottom-right-radius', 'border-bottom-style',
+            // 'border-bottom-width', 'border-collapse', 'border-color', 'border-image', 'border-image-outset', 'border-image-repeat',
+            // 'border-image-slice', 'border-image-source', 'border-image-width', 'border-left', 'border-left-color', 'border-left-style',
+            // 'border-left-width', 'border-radius', 'border-right', 'border-right-color', 'border-right-style', 'border-right-width',
+            // 'border-spacing', 'border-style', 'border-top', 'border-top-color', 'border-top-left-radius', 'border-top-right-radius',
+            // 'border-top-style', 'border-top-width', 'border-width', 'bottom', 'box-shadow', 'box-sizing',
+            // 'caption-side', 'clear', 'clip', 'color','column-count', 'column-fill', 'column-gap', 'column-rule', 'column-rule-color',
+            // 'column-rule-style', 'column-rule-width', 'column-span', 'column-width', 'columns', 'column-width', 'column-count',
+            // 'content', 'counter-increment', 'counter-reset', 'cursor',
+            // 'direction', 'display',
+            // 'empty-cells',
+            // 'flex', 'flex-basis', 'flex-direction', 'flex-flow', 'flex-direction', 'flex-wrap', 'flex-grow', 'flex-shrink',
+            // 'flex-wrap', 'float', 'font', 'font-family', 'font-size', 'font-size-adjust', 'font-stretch', 'font-style', 'font-variant',
+            // 'font-weight',
+            // 'height',
+            // 'justify', 'justify-content',
+            // 'left', 'letter-spacing', 'line-height', 'list-style', 'list-style-image', 'list-style-position', 'list-style-type',
+            // 'margin', 'margin-bottom', 'margin-left', 'margin-right', 'margin-top', 'max-height', 'max-width', 'min-height',
+            // 'min-width',
+            // 'opacity', 'order', 'outline', 'outline-color', 'outline-offset', 'outline-style', 'outline-width', 'overflow',
+            // 'overflow-x', 'overflow-y',
+            // 'padding', 'padding-bottom', 'padding-left', 'padding-right', 'padding-top', 'page-break-after', 'page-break-before',
+            // 'page-break-inside', 'perspective', 'perspective-origin', 'position',
+            // 'quotes',
+            // 'resize', 'right',
+            // 'tab-size', 'table-layout', 'text-align', 'text-align-last', 'text-align', 'text-decoration', 'text-decoration-color',
+            // 'text-decoration-line', 'text-decoration-line', 'text-decoration-style', 'text-decoration-line', 'text-indent',
+            // 'text-justify', 'text-align', 'text-overflow', 'text-shadow', 'text-transform', 'top', 'transform', 'transform-origin',
+            // 'transform-style', 'transition', 'transition-delay', 'transition-duration', 'transition-property',
+            // 'transition-timing-function',
+            // 'vertical-align', 'visibility',
+            // 'white-space', 'width', 'word-break', 'word-spacing', 'word-wrap',
+            // 'z-index'
         ],
         htmlAllowedTags: [
             'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio',
@@ -789,6 +822,7 @@ export class EditorComponent extends RootComponent implements OnInit, TriggerInt
                 buttonsVisible: 2
             }
         },
+        useClasses: false,
         videoInsertButtons: [
             'videoBack',
             '|',
