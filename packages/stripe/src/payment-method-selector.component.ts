dc93c3c3c43a0ed418b1ded4b0933b48e4850fb0
Davis <apocist@yahoo.com>
2021-07-22 17:17:34 -0700

    @stratusjs/stripe fix Selector handling data connection (loading/saving)

--- packages/stripe/src/payment-method-selector.component.ts
+++ packages/stripe/src/payment-method-selector.component.ts
@@ -1,5 +1,7 @@
 // Angular Core
 import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
     Component,
     ElementRef,
     Input,
@@ -27,7 +29,9 @@ import {Registry} from '@stratusjs/angularjs/services/registry'
 import {Collection} from '@stratusjs/angularjs/services/collection'
 import {Model} from '@stratusjs/angularjs/services/model'
 import {EventManager} from '@stratusjs/core/events/eventManager'
-import {Observable, Subject, Subscriber} from 'rxjs'
+import {Observable, ObservableInput, Subject, Subscriber, timer} from 'rxjs'
+import {catchError, debounce} from 'rxjs/operators'
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms'
 // import {EventBase} from '@stratusjs/core/events/eventBase'
 
 // Local Setup
@@ -42,6 +46,7 @@ const localDir = `${Stratus.BaseUrl}${Stratus.DeploymentPath}@stratusjs/${packag
 @Component({
     selector: `sa-${packageName}-${componentName}`,
     templateUrl: `${localDir}${componentName}.component${min}.html`,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StripePaymentMethodSelectorComponent extends RootComponent implements OnInit {
 
@@ -67,6 +72,7 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
     // Component Attributes
     @Input() property: string
     @Input() detailedBillingInfo?: boolean
+    fieldName = 'dataNumber'
 
     // Stratus Data Connectivity
     registry = new Registry()
@@ -74,22 +80,24 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
     model?: Model
     // Observable Connection
     dataSub: Observable<[]>
-    onChange = new Subject()
+    // onChange = new Subject()
     subscriber: Subscriber<any>
     // Note: It may be better to LifeCycle::tick(), but this works for now
-    dataChangeLog: string[] = []
-    // dataString = ''
-    dataNumber?: number
+    form: FormGroup = this.fb.group({
+        [this.fieldName]: new FormControl()
+    })
+    // dataChangeLog: string[] = []
 
     // Component data
     paymentCollection: Collection
-    selectedId?: number|undefined
 
     // paymentItemComponentPortal: ComponentPortal<StripePaymentMethodItemComponent>
     paymentMethodApiPath = 'PaymentMethod'
 
     constructor(
         private elementRef: ElementRef,
+        private fb: FormBuilder,
+        protected ref: ChangeDetectorRef,
         private sanitizer: DomSanitizer,
         private Stripe: StripeService,
     ) {
@@ -106,10 +114,12 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
         Stratus.Internals.CssLoader(`${localDir}${componentName}.component${min}.css`)
             .then(() => {
                 this.styled = true
+                this.refresh()
             })
             .catch(() => {
                 console.error('CSS Failed to load for Component:', this)
                 this.styled = false
+                this.refresh()
             })
 
         // TODO needs to make use of Observables
@@ -123,6 +133,7 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
         // Hydrate Root App Inputs
         this.hydrate(this.elementRef, this.sanitizer, keys<StripePaymentMethodSelectorComponent>())
 
+        // FIXME the current saved payment doesn't get reselected. may need to use 'Responsive' functions (dettach)
         // Data Connections
         this.fetchData()
             .then(data => {
@@ -130,12 +141,14 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
                     console.warn('Unable to bind data from Registry!')
                     return
                 }
+                console.log('fetchData ran', data)
                 // Manually render upon model change
                 // this.ref.detach();
                 const onDataChange = () => {
                     if (!data.completed) {
                         return
                     }
+                    console.log('onDataChange changing', data)
                     // this.onDataChange();
                     this.dataDefer(this.subscriber)
                     // Halt UI Logic when payment input is Open
@@ -148,46 +161,92 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
                 data.on('change', onDataChange)
                 onDataChange()
             })
+
+        // Declare Observable with Subscriber (Only Happens Once)
+        // this doesnt seem to do anything
+        this.dataSub = new Observable(subscriber => this.dataDefer(subscriber))
+        this.dataSub.pipe(
+            debounce(() => timer(500)),
+            catchError(this.handleObservableError)
+        ).subscribe(evt => {
+
+            // dataControl.patchValue(evt)
+            // this.selectedId = evt
+            // const dataControl = this.form.get('dataNumber')
+            const dataControl = this.form.get(this.fieldName)
+            if (dataControl.value === evt) {
+                // In the case of data being edited by the code view or something else,
+                // we need to refresh the UI, as long as it has been initialized.
+                // FIXME: This doesn't work
+                if (this.initialized) {
+                    this.refresh()
+                }
+                return
+            }
+            dataControl.patchValue(evt)
+            // console.log('dataSub env', evt)
+            this.refresh()
+        })
     }
 
     /**
      * On load, attempt to prepare and render the Card element
      */
     async ngOnInit() {
+        // const dataControl = this.form.get('dataNumber')
+        const dataControl = this.form.get(this.fieldName)
+        console.log('form', this.form)
+
+        dataControl.valueChanges.forEach(
+            (value?: number) => {
+                // Avoid saving until the Model is truly available
+                /*if (!this.model.completed) {
+                    return
+                }*/
+
+                // Save the qualified change!
+                this.valueChanged(value)
+                /*this.model.set(
+                    this.property,
+                    this.normalizeOut(innerHTML || value)
+                )*/
+            })
+
         await this.fetchPaymentMethods()
+        // TODO grey out until loaded?
         this.Stripe.registerCollection(this.paymentCollection)
         this.initialized = true
 
         // TODO need a change watcher on the selector
-        console.log('initied selector, this is model', this.model)
+        console.log('inited selector, this is model', this.model)
     }
 
     async fetchPaymentMethods() {
         await this.paymentCollection.fetch()
+        this.refresh()
     }
 
-    valueChanged() {
+    valueChanged(value: number) {
         if (
             !this.model ||
             !this.model.completed
         ) {
-            console.log('changed but no model for', this.selectedId)
+            // console.log('changed but no model for', this.selectedId)
+            console.log('changed but no model for', value)
             return
         }
-        console.log('value changed to', this.selectedId)
+        // console.log('value changed to', this.selectedId)
+        console.log('value changed to', value)
 
         // Save the qualified change!
         this.model.set(
             this.property,
-            this.normalizeOut(this.selectedId)
+            this.normalizeOut(value)
         )
     }
 
     normalizeOut(data?: number): number|null {
-        if (
-            data === null ||
-            !_.isSafeInteger(data)
-        ) {
+        if (!_.isSafeInteger(data)) {
             return null
         }
         return data
@@ -198,6 +257,7 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
         if (!data || !_.isSafeInteger(data)) {
             return null
         }
+        return data
     }
 
     // Data Connections
@@ -223,18 +283,19 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
 
     // Ensures Data is populated before hitting the Subscriber
     dataDefer(subscriber: Subscriber<any>) {
+        console.log('dataDefer running', subscriber)
         this.subscriber = this.subscriber || subscriber
         if (!this.subscriber) {
             return
         }
         // const prevString = this.dataString
-        const dataString = this.dataRef()
-        if (!dataString) {
+        const dataNumber = this.dataRef()
+        /*if (!dataNumber) {
             setTimeout(() => {
                 this.dataDefer(subscriber)
             }, 500)
             return
-        }
+        }*/
         /* if (!this.froalaConfig.useClasses) {
             if (prevString === this.dataString) {
                 return
@@ -243,18 +304,29 @@ export class StripePaymentMethodSelectorComponent extends RootComponent implemen
                 console.log('changed value pushed to subscriber:', prevString, this.dataString)
             }
         } */
-        this.subscriber.next(dataString)
+        // console.log('will run subscriber next', dataNumber)
+        this.subscriber.next(dataNumber)
         // TODO: Add a returned Promise to ensure async/await can use this defer directly.
     }
 
     dataRef(): number|null {
         if (!this.model) {
+            console.log('dataRef cant read model')
             return null
         }
         // return this.dataString = this.normalizeIn(
-        return this.dataNumber = this.normalizeIn(
+        // const blah =  this.dataNumber = this.normalizeIn(
+        return this.normalizeIn(
+        // return this.dataNumber = this.normalizeIn(
             this.model.get(this.property)
         )
+        // console.log('dataRef = ', blah, 'raw = ', this.model.get(this.property))
+        // return blah
+    }
+
+    handleObservableError(err: ObservableInput<any>): ObservableInput<any> {
+        console.error(err)
+        return err
     }
 
 }
