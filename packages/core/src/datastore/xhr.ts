d74e05447d9651b55a9c233ac38d24f07ecf86c9
alexgurrola <alexgurrola1@gmail.com>
2021-10-28 21:39:55 -0700

    Add Headers & Enhancements to `xhr` Class
    Signed-off-by: alexgurrola <alexgurrola1@gmail.com>

--- packages/core/src/datastore/xhr.ts
+++ packages/core/src/datastore/xhr.ts
@@ -1,10 +1,11 @@
 import _ from 'lodash'
-import {isJSON} from '@stratusjs/core/misc'
+import {isJSON, LooseObject} from '@stratusjs/core/misc'
 
 export interface XHRRequest {
     method?: string
     url?: string
-    data?: {} | any
+    data?: LooseObject | string
+    headers?: LooseObject
     type?: string
     success?: (response: any) => any
     error?: (response: any) => any
@@ -13,17 +14,20 @@ export interface XHRRequest {
 export class XHR {
     public method: string
     public url: string
-    public data: {} | any
+    public data: LooseObject | string | null
+    public headers: LooseObject
     public type: string
     public success: (response: any) => any
     public error: (response: any) => any
     private xhr: XMLHttpRequest
 
+    // TODO: Make the constructor optional, and allow the send to provide options instead (for class reuse)
     constructor(request?: XHRRequest) {
         // Defaults
         this.method = 'GET'
         this.url = '/Api'
-        this.data = {}
+        this.data = null
+        this.headers = {}
         this.type = ''
 
         this.success = (response: any) => {
@@ -38,23 +42,34 @@ export class XHR {
         _.extend(this, request)
     }
 
+    // todo: allow this to pass in options like the constructor
     send() {
         // Make Request
+        // TODO: Make this possibly store a const and store each reference in an array (simultaneous calls as an option)
         this.xhr = new XMLHttpRequest()
-        const promise: any = new Promise((resolve: any, reject: any) => {
+        const promise = new Promise((resolve, reject) => {
             this.xhr.open(this.method, this.url, true)
+
             if (typeof this.type === 'string' && this.type.length) {
                 this.xhr.setRequestHeader('Content-Type', this.type)
             }
+
+            if (typeof this.headers === 'object' && this.headers && this.headers.length) {
+                _.forEach(this.headers, (v, k) => this.xhr.setRequestHeader(k, v))
+            }
+
             this.xhr.onload = () => {
+                // todo: make this 1 deep
                 if (this.xhr.status >= 200 && this.xhr.status < 400) {
                     let response: any = this.xhr.responseText
                     if (isJSON(response)) {
                         response = JSON.parse(response)
                     }
                     resolve(response)
+                    return
                 } else {
                     reject(this.xhr)
+                    return
                 }
             }
 
@@ -62,11 +77,25 @@ export class XHR {
                 reject(this.xhr)
             }
 
-            if (Object.keys(this.data).length) {
-                this.xhr.send(JSON.stringify(this.data))
-            } else {
+            // allow straight calls
+            if (!this.data) {
                 this.xhr.send()
+                return
             }
+
+            // serialize data types
+            if (typeof this.data === 'string' && this.data.length) {
+                this.xhr.send(this.data)
+                return
+            }
+
+            if (typeof this.data === 'object' && Object.keys(this.data).length) {
+                this.xhr.send(JSON.stringify(this.data))
+                return
+            }
+
+            // catch remaining
+            this.xhr.send()
         })
         promise.then(this.success, this.error)
         return promise
