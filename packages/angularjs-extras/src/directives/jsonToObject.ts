a1afd04180896ab3a3434506279cafafa95496a2
Davis <apocist@yahoo.com>
2022-03-21 15:41:01 -0700

    @stratusjs/angular-extras/directives/jsonToObject

---
+++ packages/angularjs-extras/src/directives/jsonToObject.ts
@@ -0,0 +1,42 @@
+// JSON to Object Directive
+// -----------------
+
+// Runtime
+import _ from 'lodash'
+import {Stratus} from '@stratusjs/runtime/stratus'
+
+// Angular 1 Modules
+import {isJSON} from '@stratusjs/core/misc'
+import {IScope, INgModelController} from 'angular'
+
+
+// Environment
+// const min = !cookie('env') ? '.min' : ''
+const packageName = 'angularjs-extras'
+// const moduleName = 'directives'
+const componentName = 'jsonToObject'
+
+Stratus.Directives.JsonToObject = () => ({
+    restrict: 'A',
+    require: 'ngModel',
+    link: (
+        $attrs: angular.IAttributes,
+        $element: angular.IAugmentedJQuery,
+        $scope: IScope & any,
+        ngModel: INgModelController
+    ) => {
+        const $ctrl: any = this
+        $ctrl.uid = _.uniqueId(_.camelCase(packageName) + '_' + _.camelCase(componentName) + '_')
+        Stratus.Instances[$ctrl.uid] = $scope
+
+        $ctrl.convertToJsonString = (val: object) => !_.isString(val) && !_.isNumber(val) ? JSON.stringify(val) : val
+
+        $ctrl.convertToModelObject = (val: string) =>  isJSON(val) ? JSON.parse(val) : val
+
+        // When model data changes, this will convert any objects into stringified json so html elements can display it
+        ngModel.$formatters.push((modelValue: object) => $ctrl.convertToJsonString(modelValue))
+
+        // When the html element changes, this will convert any json into object for model use
+        ngModel.$parsers.push((viewValue: string) => $ctrl.convertToModelObject(viewValue))
+    }
+})
