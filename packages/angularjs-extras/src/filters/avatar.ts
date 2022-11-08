76356d4338fa636f791f7bbb7749d4fc24a31f96
Davis <apocist@yahoo.com>
2022-11-07 16:43:20 -0800

    gavatar filter converted

--- packages/angularjs-extras/src/filters/avatar.ts
+++ packages/angularjs-extras/src/filters/avatar.ts
@@ -14,3 +14,15 @@ Stratus.Filters.Avatar = () => (
     fallback = fallback || 'retro' // use wording 'fallback' as 'default' is reserved
     return `//seccdn.libravatar.org/avatar/${md5(input.trim().toLowerCase())}?s=${size}&d=${fallback}`
 }
+
+/*
+    // Old Gravatar old moved to avatar
+    Stratus.Filters.Gravatar = function () {
+        return function (input) {
+            if (!input) {
+                return '//www.gravatar.com/avatar/'
+            }
+            return '//www.gravatar.com/avatar/' + md5(input.trim().toLowerCase())
+        }
+    }
+*/
