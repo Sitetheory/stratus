a860dd1b07546d4dfe40925f723b5c42aa9e2991
Davis <apocist@yahoo.com>
2022-11-07 16:30:05 -0800

    toArray filter converted

--- packages/angularjs-extras/src/filters/toArray.ts
+++ packages/angularjs-extras/src/filters/toArray.ts
@@ -12,20 +12,20 @@ import * as angular from 'angular'
 /**
  * @author https://github.com/petebacondarwin/angular-toArrayFilter
  */
-Stratus.Filters.ToArray = () => {
-    return (obj: LooseObject, addKey?: boolean) => {
-        if (!angular.isObject(obj)) return obj
-        if (addKey === false) {
-            return Object.keys(obj).map((key) => {
-                return (obj as LooseObject)[key]
-            })
-        } else {
-            return Object.keys(obj).map((key) => {
-                const value = (obj as LooseObject)[key]
-                return angular.isObject(value) ?
-                    Object.defineProperty(value, '$key', { enumerable: false, value: key }) :
-                    { $key: key, $value: value }
-            })
-        }
+Stratus.Filters.ToArray = () => (
+    obj: LooseObject, addKey?: boolean
+) => {
+    if (!angular.isObject(obj)) return obj
+    if (addKey === false) {
+        return Object.keys(obj).map((key) => {
+            return (obj as LooseObject)[key]
+        })
+    } else {
+        return Object.keys(obj).map((key) => {
+            const value = (obj as LooseObject)[key]
+            return angular.isObject(value) ?
+                Object.defineProperty(value, '$key', { enumerable: false, value: key }) :
+                { $key: key, $value: value }
+        })
     }
 }
