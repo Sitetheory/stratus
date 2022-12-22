289e310a4dc3225aeffd6c7e3d291f5c7a94b7fa
Davis <apocist@yahoo.com>
2022-12-21 17:10:03 -0800

    @stratusjs/citation add initial checks and warnings for content

--- packages/angularjs-extras/src/components/citation.ts
+++ packages/angularjs-extras/src/components/citation.ts
@@ -1,19 +1,17 @@
 // Runtime
-import {camelCase, uniqueId} from 'lodash'
 import {Stratus} from '@stratusjs/runtime/stratus'
-import * as angular from 'angular'
-
-// Angular 1 Modules
-import 'angular-material'
+import {IAugmentedJQuery, IScope, ITranscludeFunction} from 'angular'
 
 // Stratus Dependencies
-import {LooseFunction, LooseObject} from '@stratusjs/core/misc'
+import {safeUniqueId} from '@stratusjs/core/misc'
 import {cookie} from '@stratusjs/core/environment'
 
-export type CitationComponentScope = angular.IScope & LooseObject<LooseFunction> & {
+export type CitationComponentScope = IScope & {
+    // testId: string
     uid: string
-    title: string
-    content: string // HTML
+    initialized: boolean
+    // title: string
+    // content: string // HTML
     citationOpened: boolean
     auto: boolean
 
@@ -29,27 +27,36 @@ const localDir = `${Stratus.BaseUrl}${Stratus.DeploymentPath}@stratusjs/${packag
 
 Stratus.Components.Citation = {
     transclude: {
-        content: 'stratusCitationContent',
+        content: '?stratusCitationContent',
         title: '?stratusCitationTitle',
     },
     controller(
         $scope: CitationComponentScope,
-        $transclude: angular.ITranscludeFunction
+        $element: IAugmentedJQuery,
+        $transclude: ITranscludeFunction,
+        // $attrs: IAttributes
     ) {
         // Initialize
-        const $ctrl = this
-        $scope.uid = uniqueId(camelCase(packageName) + '_' + camelCase(moduleName) + '_' + camelCase(componentName) + '_')
+        $scope.uid = safeUniqueId(packageName, moduleName, componentName)
         Stratus.Instances[$scope.uid] = $scope
         $scope.auto = true
+        $scope.initialized = false
+        // $scope.testId = $attrs.testId || '0'
 
-        Stratus.Internals.CssLoader(`${localDir}${componentName}${min}.css`)
+        Stratus.Internals.CssLoader(`${localDir}${componentName}${min}.css`).then()
 
         // Initialization
-        $ctrl.$onInit = () => {
+        this.$onInit = () => {
+            if (!$transclude.isSlotFilled('content')) {
+                console.warn('Warning, stratus-citation had no content', $scope, $element)
+                return // don't load
+            }
             if ($transclude.isSlotFilled('title')) {
                 // We need to use the custom title
                 $scope.auto = false
             }
+            $scope.$applyAsync(() => {$scope.initialized = true})
+            // console.log('initted', $scope, $element)
         }
         $scope.toggleCitation = () => {
             $scope.citationOpened = !$scope.citationOpened
