4695bfeaed64ee3de6a03eec2361e81a0f420b55
Davis <apocist@yahoo.com>
2023-02-07 11:03:17 -0800

    @stratusjs/stripe 1.5.0 Stripe StratusPackage setup

---
+++ packages/stripe/src/stripe.module.ts
@@ -0,0 +1,45 @@
+import {StratusPackage} from '@stratusjs/angular/app.module'
+import { NgModule } from '@angular/core'
+import {StripePaymentMethodComponent} from './payment-method.component'
+import {StripePaymentMethodItemComponent} from './payment-method-item.component'
+import {StripePaymentMethodListComponent} from './payment-method-list.component'
+import {StripePaymentMethodSelectorComponent} from './payment-method-selector.component'
+import {StripeSetupIntentComponent} from './setup-intent.component'
+
+
+@NgModule({
+    // These determine what exists as a component within Angular system.
+    declarations: [
+        StripePaymentMethodComponent,
+        StripePaymentMethodItemComponent,
+        StripePaymentMethodListComponent,
+        StripePaymentMethodSelectorComponent,
+        StripeSetupIntentComponent,
+    ],
+    // This determines what is accessible via DOM as a component. These must be listed in `declarations`. (required in stratus)
+    entryComponents: [
+        StripePaymentMethodComponent,
+        StripePaymentMethodItemComponent,
+        StripePaymentMethodListComponent,
+        StripePaymentMethodSelectorComponent,
+        StripeSetupIntentComponent,
+    ],
+    exports: [
+        StripePaymentMethodComponent,
+        StripePaymentMethodItemComponent,
+        StripePaymentMethodListComponent,
+        StripePaymentMethodSelectorComponent,
+        StripeSetupIntentComponent,
+    ],
+    providers: [
+        {provide: Window, useValue: window}
+    ]
+})
+export class StripeModule {}
+export const StripePackage: StratusPackage = {
+    stratusModule: StripeModule,
+    stratusComponents: {
+        'sa-stripe-payment-method-list': StripePaymentMethodListComponent,
+        'sa-stripe-payment-method-selector': StripePaymentMethodSelectorComponent,
+    }
+}
