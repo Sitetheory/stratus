51091909a356f4b8588405d8f296de463b7276b0
Davis <apocist@yahoo.com>
2023-03-08 17:01:02 -0800

    @stratusjs/angular 0.6.2

--- packages/angularjs-extras/src/normalizers/angular.exports.js
+++ packages/angularjs-extras/src/normalizers/angular.exports.js
@@ -6,7 +6,7 @@
 // Ensure angular gets exported appropriately
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['angular-native'], factory)  // FIXME defining 'angular-native' here can attempt to load angularjs a second time. Need to import instead
+    define(['angular-native'], factory) // FIXME defining 'angular-native' here can attempt to load angularjs a second time. Need to import instead
   } else {
     factory()
   }
