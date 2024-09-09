b1b1056dde8251a9d1f3fccd1ff6900d3d4cf9ba
Davis <apocist@yahoo.com>
2024-09-09 12:38:56 -0700

    @stratusjs/angular-extras remove unused

--- packages/angularjs-extras/src/filters/reduce.ts
+++ packages/angularjs-extras/src/filters/reduce.ts
@@ -5,12 +5,12 @@ import {
     Stratus
 } from '@stratusjs/runtime/stratus'
 import {reduce} from 'lodash'
-import * as angular from 'angular'
+import {IInterpolateService} from 'angular'
 
 // This filter applies a reduce to an array or object
 // FIXME: This is terribly malfunctioning (old note. no examples of usage found)
 Stratus.Filters.Reduce = (
-    $interpolate: angular.IInterpolateService,
+    $interpolate: IInterpolateService,
 ) => {
     return (input: any, expression?: string) => {
         return input || reduce(input, (value) => {
